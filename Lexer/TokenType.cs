b131ce94578dc19ab30b2f1b78ceee0cde3e2a30
Jirka <pisejiricz@gmail.com>
2022-10-29 15:06:23 +0200

    Lexer

---
+++ Lexer/TokenType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemPrace_BTEJA_BCSH2
+{
+    public enum TokenType
+    {
+        Let, Function, If, Else, While, Return,
+        Int, Double, String, Boolean, Void,
+        Equal_Equal, Not_Equal, Greater, Less, Greater_Equal, Less_Equal,
+        Plus, Minus, Slash, Star,
+        Dot, Colon, Semi_Colon, Comma, Equal,
+        Left_Bracket, Right_Bracket, Left_Curly_Bracket, Right_Curly_Bracket,
+        True, False,
+        Ident, Number, Real_Number, String_Value
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SemPrace_BTEJA_BCSH2;
+
+string input;
+
+using (StreamReader sr = new StreamReader("input.txt")) 
+{
+    input = sr.ReadToEnd();
+}
+
+Lexer lexer = new Lexer();
+List<Token> tokens = lexer.ScanTokens(input);
+
+foreach (Token token in tokens)
+{
+    Console.WriteLine(token);
+}
